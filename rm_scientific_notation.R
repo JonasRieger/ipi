863856a00c879eb846de36fb54cb7d9c6b1a3064
JonasRieger <jonas.rieger@tu-dortmund.de>
2022-05-17 09:46:42 +0200

    typo

--- rm_scientific_notation.R
+++ rm_scientific_notation.R
@@ -1,3 +1,16 @@
 library(data.table)
 setwd("analysis")
 
+a = fread("ipi.csv")
+a[, ipi := format(ipi, scientific = F)]
+
+fwrite(a, "ipi_dec.csv")
+
+
+a = fread("topics_rel_to_entire.csv")
+a = format(a, scientific = F)
+
+fwrite(a, "topics_rel_to_entire_dec.csv")
+
+
+
