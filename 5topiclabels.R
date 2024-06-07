b01dcd67acbb705eaefe759e4018e8befe174a93
JonasRieger <jonas.rieger@tu-dortmund.de>
2024-06-07 08:10:14 +0200

    May 2024

---
+++ 5topiclabels.R
@@ -0,0 +1,16 @@
+library(data.table)
+devtools::load_all("../topiclabels")
+token = ""
+
+files = list.files("analysis/topWordsPerMonth/", full.names = TRUE)
+
+a = fread(files[4])[,-1]
+dates = as.Date(as.character(a[1,]))
+a = unname(as.matrix(a[-c(1:3),]))
+
+b = label_topics(a, token = token)
+b = label_topics(a, token = token, context = "Inflation Perception")
+
+d = rbind(as.character(dates), b$labels, a)
+write.table(d, "EmergingMarketsLabels.csv", sep = ",",
+            row.names = FALSE, col.names = FALSE)
